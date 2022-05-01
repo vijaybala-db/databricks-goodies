0b8ac5ae3511addf3ea20786465542f0417318c5
vbalasu <vbalasu>
2022-05-01 03:53:25 +0000

    Added location.json ingest

--- location-tracker/01_ingest.py
+++ location-tracker/01_ingest.py
@@ -4,4 +4,40 @@
 
 # COMMAND ----------
 
+!aws s3 ls s3://databricks-partners/
+
+# COMMAND ----------
+
+# MAGIC %pip install geocoder
+
+# COMMAND ----------
+
+import datetime, json
+import geocoder
+g = geocoder.ip('me')
+data = {'when': datetime.datetime.utcnow().isoformat(), 'latitude': g.latlng[0], 'longitude': g.latlng[1]}
+json.dumps(data)
+
+# COMMAND ----------
+
+with open('/dbfs/tmp/location.json', 'w') as f:
+  f.write(json.dumps(data))
+
+# COMMAND ----------
+
+!aws s3 cp /dbfs/tmp/location.json s3://databricks-partners/location/location.json
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC --DROP TABLE vijay_b.location;
+# MAGIC CREATE TABLE vijay_b.location USING JSON LOCATION '/tmp/location.json';
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC SELECT * FROM vijay_b.location;
+
+# COMMAND ----------
+
 
