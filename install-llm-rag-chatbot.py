bcf038acd60cd01e1c24e61e58a7bad56aa2eca4
vijaybala-db <vijaybala-db>
2024-01-15 01:49:17 +0000

    Added llm-rag-chatbot to install in main.rag_chatbot_vbalasu

---
+++ install-llm-rag-chatbot.py
@@ -0,0 +1,11 @@
+# Databricks notebook source
+# MAGIC %pip install dbdemos
+
+# COMMAND ----------
+
+import dbdemos
+dbdemos.install('llm-rag-chatbot', catalog='main', schema='rag_chatbot_vbalasu', use_current_cluster=True)
+
+# COMMAND ----------
+
+
